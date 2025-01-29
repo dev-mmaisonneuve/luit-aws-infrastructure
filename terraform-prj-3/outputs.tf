b7f35f1d61deff22d91fc8aae71381b88c6e66ec
Maïco M. <168045109+dev-mmaisonneuve@users.noreply.github.com>
2025-01-28 19:42:18 -0500

    Added output for RDS Endpoint

--- terraform-prj-3/outputs.tf
+++ terraform-prj-3/outputs.tf
@@ -7,3 +7,7 @@ output "web_2_public_ip" {
   description = "Public IP of the second web server"
   value       = aws_instance.web_2.public_ip
 }
+output "rds_endpoint" {
+  description = "Endpoint of the RDS MySQL instance"
+  value       = aws_db_instance.rds_mysql.endpoint
+}
